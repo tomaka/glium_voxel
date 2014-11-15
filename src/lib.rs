0951d87b966e66530a77099cdeb48b4cfde18712
Pierre Krieger <pierre.krieger1708@gmail.com>
2014-11-15 09:31:03 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#![deny(missing_docs)]
+#![feature(globs)]
+
+//! A voxel rendering library on top of Glium.
+
+extern crate glium;
+extern crate image;
+extern crate vecmath;
+
+pub mod array;
+pub mod cube;
+pub mod texture;
